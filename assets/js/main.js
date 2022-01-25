6b8c8a7a7af8863baf1391e2f804c705760fffa7
Marco <marco_broter@hotmail.com>
2022-01-25 02:03:45 -0500

    actulizacion productos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,22 @@
-(()=>{
-    const offArrowoffBtnTopClose=document.querySelector('#offBtnTopClose');
-    const offArrow=document.querySelector('#offArrow');
+;(() => {
+	const offArrow = document.getElementById('offArrow')
+	const offArrowoffBtnTopClose = document.querySelector('#offBtnTopClose')
+	console.log('offArrow0' + offArrowoffBtnTopClose)
 
-    offArrow.addEventListener('click',()=>{
-        offArrowoffBtnTopClose.classList.toggle('offBtnTopClose');
-    })
-})();
+	offArrow.addEventListener('click', (e) => {
+		console.log(offArrow)
+		console.log('offArrow0')
+		e.preventDefault
+		console.log('offArrow0', offArrow)
+		offArrowoffBtnTopClose.classList.toggle('offBtnTopClose')
+	})
+})()
+;(() => {
+	const idCategoria = document.querySelector('#idCategoria')
+	const btnCategoria = document.querySelector('#btnCategoria')
+
+	btnCategoria.addEventListener('click', () => {
+		btnCategoria.classList.toggle('show-traslation-boton')
+		idCategoria.classList.toggle('show-traslation')
+	})
+})()
